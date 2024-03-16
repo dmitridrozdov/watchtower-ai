82c48313f451e5697b68f5f66d82cabe02d53528
dmitridrozdov <dm.drozdov@gmail.com>
2024-03-16 12:01:51 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,37 @@
 "use client"
 
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Webcam from 'react-webcam';
 
 type Props = {}
 
 const HopePage = (props: Props) => {
   const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  // state 
+  const [mirrored, setMirrored] = useState<boolean>(true);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false)
+  const [volume, setVolume] = useState(0.8);
+  // const [model, setModel] = useState<ObjectDetection>();
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>HopePage</div>
+    <div className='flex h-screen'>
+      {/* Left division - webcam and Canvas  */}
+      <div className='relative'>
+        <div className='relative h-screen w-full'>
+          {/* <Webcam ref={webcamRef}
+            mirrored={mirrored}
+            className='h-full w-full object-contain p-2'
+          /> */}
+          <canvas ref={canvasRef}
+            className='absolute top-0 left-0 h-full w-full object-contain'
+          ></canvas>
+        </div>
+      </div>
+    </div>
   )
 }
 
